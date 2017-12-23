d5d78ca6e742a568da7d57123ae27b8cdafb542b
iFew <chitpong@gmail.com>
2017-12-23 20:46:58 +0700

    Added insert data and get data from database

---
+++ src/api/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using api.Models;
+
+namespace api.Controllers
+{
+    [Route("[controller]")]
+    public class LoginController : Controller
+    {
+        private readonly UserContext _context;
+
+        public LoginController(UserContext context)
+        {
+            _context = context;
+
+            if (_context.Users.Count() == 0)
+            {
+                _context.Users.Add(new User { Id = 1, Username = "few", Password = "1234", Displayname = "iFew" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<User> GetAll()
+        {
+            return _context.Users.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(long id)
+        {
+            var item = _context.Users.FirstOrDefault(t => t.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+    }
+}
