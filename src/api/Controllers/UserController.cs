2bee5d0544ea70f0a87ab4f596551d5fe8751802
iFew <chitpong@gmail.com>
2017-12-24 16:07:06 +0700

    Added User controller

---
+++ src/api/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using api.Models;
+
+namespace api.Controllers
+{
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+    }
+    
+}
